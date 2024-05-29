05aa49a1a0be1ba1a58acd48a1c17cf3d1e58b37
Sara Martín <sara.04.sm@gmail.com>
2024-05-29 14:51:51 +0200

    changes to plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -2,6 +2,29 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def plot_bars(df,title,file_path):
+    df_melted = df.reset_index().melt(id_vars='index', var_name='Execution', value_name='Value')
+    df_melted.rename(columns={'index': 'Step'}, inplace=True)
+
+    sns.set(style='whitegrid')
+    plt.figure(figsize=(15, 9))
+
+    # Create the bar plot
+    sns.barplot(data=df_melted, x='Step', y='Value', hue='Execution', palette='Paired')
+
+    # Add labels and title
+    plt.xlabel('Step')
+    plt.ylabel('Value')
+    plt.title(title)
+
+    # Save png
+    plt.savefig(file_path, format='png')
+
+    # Show the plot
+    plt.show()
+
+
+
 def plot_hyperparam(df, title, file_path):
 
     df_melted = df.reset_index().melt(id_vars='index', var_name='Execution', value_name='Value')
@@ -33,47 +56,32 @@ def drop_columns(df):
 
 
 def read_files(language):
-    # Train
-    trainloss = pd.read_csv(f'/models/{language}/wandb_graphs/train_loss.csv')
-    trainacc = pd.read_csv(f'/models/{language}/wandb_graphs/train_acc.csv')
-    trainwer = pd.read_csv(f'/models/{language}/wandb_graphs/train_wer.csv')
-    trainper = pd.read_csv(f'/models/{language}/wandb_graphs/train_per.csv')
+    print(language)
 
-    for df, title in zip([trainloss,trainacc,trainwer,trainper],
-        ["Train loss", "Train accuracy", "Train WER", "Train PER"]):
-            plot_hyperparam(df, title)
 
-    # Validation
-    valloss = pd.read_csv(f'/models/{language}/wandb_graphs/val_loss.csv')
-    valacc = pd.read_csv(f'/models/{language}/wandb_graphs/val_acc.csv')
-    valwer = pd.read_csv(f'/models/{language}/wandb_graphs/val_wer.csv')
-    valper = pd.read_csv(f'/models/{language}/wandb_graphs/val_per.csv')
+    for data in ["train","val","test"]:
 
-    for df, title in zip([valloss,valacc,valwer,valper],
-        ["Validation loss", "Validation accuracy", "Validation WER", "Validation PER"]):
-            plot_hyperparam(df, title)
+        for graph in ["loss","acc","wer","per"]:
 
-    # Test
-    testloss = pd.read_csv(f'/models/{language}/wandb_graphs/test_loss.csv')
-    testacc = pd.read_csv(f'/models/{language}/wandb_graphs/test_acc.csv')
-    testwer = pd.read_csv(f'/models/{language}/wandb_graphs/test_wer.csv')
-    testper = pd.read_csv(f'/models/{language}/wandb_graphs/test_per.csv')
+            try:
+                print(f'../models/{language}/wandb_graphs/{data}_{graph}.csv')
+                df = pd.read_csv(f'../models/{language}/wandb_graphs/{data}_{graph}.csv')
+                df = drop_columns(df)
+                if data == "test":
+                    plot_bars(df,title=f"{data} {graph}", file_path=f"./images/{language}/{data}_{graph}")
+                else:
+                    plot_hyperparam(df, title=f"{data} {graph}", file_path=f"./images/{language}/{data}_{graph}")
 
-    for df, title in zip([testloss,testacc,testwer,testper],
-        ["Test loss", "Test accuracy", "Test WER", "Test PER"]):
-            plot_hyperparam(df, title)
-    
+            except:
+                print(f"CSV of {data} {graph} not found.") 
 
 
 if __name__ == "__main__":
     # Print plots of hyperparameter tuning
-    # Read files
-    try:
-        read_files("eng-spa")
-        read_files("spa-eng")
-    except:
-        print("Error. Some file is missing or name of file is incorrect.")
     
+    read_files(language="eng-spa")
+    read_files(language="spa-eng")
+
     """
     CAMBIAR PLOT TEST A BARS?
     CHANGE PLOT SO THAT IT SAVES THE PNG AUTOMATICALLY
